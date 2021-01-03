79e2f62d85987759ca2cf241bfbc2d536dce8abb
Firas Zouari <firas0015.zouari@gmail.com>
2021-01-03 15:22:36 +0100

    Add project files.

---
+++ BlazorApp/Pages/EmployeeListBase.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeClassLibrary;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            LoadEmployees();
+            return base.OnInitializedAsync();
+        }
+        private void LoadEmployees()
+        {
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "David@pragimtech.com",
+                DateOfBirth = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department
+                {
+                    DepartmentId = 1,
+                    DepartmentNames = "IT"
+                },
+                PhotoPath = "images/john.png"
+            };
+        }
+    }
+}
